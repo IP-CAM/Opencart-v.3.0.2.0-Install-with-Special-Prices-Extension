3b9f83ae477947a4387ae53162024a698d7562e0
Armandas <armandas.motuzas@gmail.com>
2017-08-06 14:42:11 +0300

    First custom hello world module

---
+++ admin/language/en-gb/extension/module/special_prices.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Special prices';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified Hello World module!';
+$_['text_edit']        = 'Edit Hello World module';
+$_['text_extension']   = 'Extensions';
+
+// Entry
+$_['entry_code']       = 'Message';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Hello World module!';
+$_['error_code']       = 'Text Required';
